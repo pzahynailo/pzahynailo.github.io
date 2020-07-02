fe0706531353674cc2d0ea3361cca94eca3911e2
Pavel Zahynailo <pzag97@gmail.com>
2020-07-03 01:37:06 +0300

    Initial Commit

---
+++ precache-manifest.9ff030c4688df53a3a4c8bdea4f1dd18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd28b91bed70b409202bac1b5ca1e579",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65b62b47bd1f729ca02d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b52e72fe440d5e27f655",
+    "url": "/static/js/2.bb31fa59.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.bb31fa59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65b62b47bd1f729ca02d",
+    "url": "/static/js/main.3584b0e3.chunk.js"
+  },
+  {
+    "revision": "8f250f8e39271b6297a7",
+    "url": "/static/js/runtime-main.3de06ba6.js"
+  }
+]);
